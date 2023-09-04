938998079d9764428354b636721530f6bdc16b48
jjeongsu <jeongsu.dev@gmail.com>
2023-09-04 16:35:33 +0900

    2.0 useReducer

--- react-study/src/components/Counter.tsx
+++ react-study/src/components/Counter.tsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
+interface actionType {
+  type : string;
+}
+interface stateType {
+  value: number;
+}
+function reducer(state:stateType, action: actionType){
+  //action, type에 따라 새로운 상태를 반환해줌
+  console.log(typeof(action));
+  console.log(state);
+  switch(action.type){
+    case 'INCREMENT':
+      return {value : state.value +1}
+    case 'DECREMENT':
+      return {value : state.value -1}
+    default:
+      return state
+  }
+}
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {value : 0});
+  //state는 현재 가리키고 있는 상태, dispatch는 액션을 발생시키는 함수
+  //dispatch(action) 형태로 함수안에 파라미터로 액션값을 넣어주면 -> 리듀서 함수 호출
 
   console.log('Counter 컴포넌트 랜더링')
   return (
     <div>
-      <p>현재 카운터의 값은 {value} 입니다.</p>
-      <button onClick={() => {setValue(prev => prev+1)}}>+1</button>
-      <button onClick={() => {setValue(prev => prev-1)}}>-1</button>
+      <p>현재 카운터의 값은 {state.value} 입니다.</p>
+      <button onClick={() => {dispatch({type : "INCREMENT"})}}>+1</button>
+      <button onClick={() => {dispatch({type : 'DECREMENT'})}}>-1</button>
     </div>
   )
 }
--- react-study/src/components/Info.tsx
+++ react-study/src/components/Info.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state: any, action: any) => {
+  return {
+    ...state, 
+    [action.name]: action.value,
+  }
+}
 
 const Info  = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  const onChangeName = (e:any) => {
-    setName(e.target.value);
-  }
-  
-  const onChangeNickname = (e:any) => {
-    setNickname(e.target.value);
-  }
+  const [state, dispatch] = useReducer(reducer, {name:"", nickname:''});
+  const {name, nickname} = state;
+  const onChange = (e:any) => {dispatch(e.target)}
   console.log('info 컴포넌트 랜더링');
   return (
     <div>
-      <input onChange={onChangeName} value={name}/>
-      <input onChange={onChangeNickname} value={nickname} />
+      <input onChange={onChange} value={name} name="name"/>
+      <input onChange={onChange} value={nickname} name="nickname"/>
 
     <p>이름 : {name}</p>
     <p>닉네임 : {nickname}</p>
